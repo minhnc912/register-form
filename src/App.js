a9ad8e02f7a0b16a6af1283b66d1b3fff2923b09
minhnc912 <minhnguyen91200@gmail.com>
2023-04-10 12:48:32 +0700

    register-form finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [submitted, setSubmitted] = useState(false);
+    const [valid, setValid] = useState({
+        firstName: { valid: false, msg: "" },
+        lastName: { valid: false, msg: "" },
+        email: { valid: false, msg: "" },
+    });
+    const mailFormat = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    const firstNameHandler = (event) => {
+        setFirstName(event.target.value);
+    };
+
+    const lastNameHandler = (event) => {
+        setLastName(event.target.value);
+    };
+
+    const emailHandler = (event) => {
+        setEmail(event.target.value);
+    };
+    debugger;
+    const registerSubmitHandler = (event) => {
+        event.preventDefault();
+        if (!validate()) {
+            setSubmitted(true);
+            reset();
+        }
+    };
+
+    const reset = () => {
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+    };
+
+    const validate = () => {
+        const temp = {
+            firstName: { valid: false, msg: "" },
+            lastName: { valid: false, msg: "" },
+            email: { valid: false, msg: "" },
+        };
+        if (!firstName) {
+            temp.firstName = { valid: true, msg: "Please enter a first name" };
+        }
+        if (!lastName) {
+            temp.lastName = {
+                valid: true,
+                msg: "Please enter a last name",
+            };
+        }
+        if (!email) {
+            temp.email = {
+                valid: true,
+                msg: "Please enter an email address",
+            };
+        } else if (!email.match(mailFormat)) {
+            temp.email = {
+                valid: true,
+                msg: "Please enter an valid email address (example: test@gmail.com)",
+            };
+        }
+        setValid(temp);
+        return !temp.firstName && !temp.lastName && !temp.email;
+    };
+
+    return (
+        <div className="form-container" onSubmit={registerSubmitHandler}>
+            <form className="register-form">
+                {submitted ? (
+                    <div className="success-message">
+                        Success! Thank you for registering
+                    </div>
+                ) : null}
+                <input
+                    id="first-name"
+                    className="form-field"
+                    type="text"
+                    placeholder="First Name"
+                    name="firstName"
+                    value={firstName}
+                    onChange={firstNameHandler}
+                />
+                {valid.firstName.valid && (
+                    <span id="first-name-error">{valid.firstName.msg}</span>
+                )}
+                <input
+                    id="last-name"
+                    className="form-field"
+                    type="text"
+                    placeholder="Last Name"
+                    name="lastName"
+                    value={lastName}
+                    onChange={lastNameHandler}
+                />
+                {valid.lastName.valid && (
+                    <span id="first-name-error">{valid.lastName.msg}</span>
+                )}
+                <input
+                    id="email"
+                    className="form-field"
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    value={email}
+                    onChange={emailHandler}
+                />
+                {valid.email.valid && (
+                    <span id="first-name-error">{valid.email.msg}</span>
+                )}
+                <button className="form-field" type="submit">
+                    Register
+                </button>
+            </form>
+        </div>
+    );
 }
-
-export default App;
